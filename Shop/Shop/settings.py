7fc6bea7ae62e5a2d44694efc1e3836058414bca
styse01 <shmadad0@gmail.com>
2023-03-13 11:45:47 +0330

    Reference database information to .env file

--- Shop/Shop/settings.py
+++ Shop/Shop/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 

f046b19dd1fc5b7a37f82f44761e3c3bdeb881ee
styse01 <shmadad0@gmail.com>
2023-03-13 11:47:24 +0330

    Reference secret_key to the .env file

--- Shop/Shop/settings.py
+++ Shop/Shop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^41(l1daw1+o3rwu)=8gpeud71y8iw^il+zh+%frn=w#@+$qk('
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
